ce0ba275a29b7917f2a186bc725635e0df38adf0
AryanSinghalGit <aryan.singhal@successive.tech>
2020-06-09 14:30:14 +0530

    add mutations

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,13 +1,15 @@
 import { mergeTypes, fileLoader } from 'merge-graphql-schemas';
 import path from 'path';
 import { query } from './user';
+import * as trainee from './trainee';
 
 const typesArray = fileLoader(path.join(__dirname, './**/*.graphql'));
 const typeDefs = mergeTypes(typesArray, { all: true });
 
 export default {
   resolvers: {
-    Query: { ...query },
+    Query: { ...query, ...trainee.query },
+    Mutation: {...trainee.mutation}
   },
   typeDefs
 };
---
+++ src/service/user.js
@@ -0,0 +1,33 @@
+class User {
+  constructor() {
+    this.users = new Map();
+    this.id = 0;
+  }
+
+  createUser = (user) => {
+    this.id++;
+    this.users.set(this.id, { ...user, id: this.id });
+    return this.users.get(this.id);
+  }
+
+  getAllUsers = () => {
+    let users = [];
+    for (let value of this.users.values()) {
+      users = [...users, value]
+    }
+    return users;
+  }
+
+  updateUser = (id, rest) => {
+    const user = this.users.get(Number(id));
+    this.users.set(Number(id), { ...user, ...rest });
+    return this.users.get(Number(id));
+  }
+
+  deleteUser = (id) => {
+    this.users.delete(Number(id));
+    return id;
+  }
+}
+
+export default new User();
