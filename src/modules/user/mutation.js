a71de2045942c8ae835cd3d48f789f87ed507c8c
AryanSinghalGit <aryan.singhal@successive.tech>
2020-06-11 14:55:27 +0530

    add error handling

--- src/modules/user/mutation.js
+++ src/modules/user/mutation.js
@@ -1,9 +1,17 @@
+import { ApolloError } from 'apollo-server';
+
 const mutation = {
   loginUser: async (root, args, context) => {
     const { email, password } = args;
     const { dataSources: { userAPI } } = context;
-    const { data } = await userAPI.loginUser({ email, password });
-    return data;
+    try {
+      const { data } = await userAPI.loginUser({ email, password });
+      return data;
+    }
+    catch (err) {
+      const { message, status } = err.extensions.response.body;
+      throw new ApolloError(message, status);
+    }
   },
 };
 
