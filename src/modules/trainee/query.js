ec0d7451cdc4b94bd4e4b8454c485f5d69f454bd
AryanSinghalGit <aryan.singhal@successive.tech>
2020-06-12 17:10:46 +0530

    add context and trainee data source

--- src/modules/trainee/query.js
+++ src/modules/trainee/query.js
@@ -1,8 +1,17 @@
-import { userInstance } from '../../service/index';
+import { ApolloError } from "apollo-server";
 
 const query = {
-  getAllTrainees: () => {
-    return userInstance.getAllUsers();
+  getAllTrainees: async (root, args, context) => {
+    try {
+      const { skip = 0, limit = 20 } = args;
+      const { dataSources: { traineeAPI } } = context;
+      const { data } = await traineeAPI.getAllTrainees({ skip, limit });
+      return data;
+    }
+    catch (err) {
+      const { message, status } = err.extensions.response.body;
+      throw new ApolloError(message, status);
+    }
   },
 };
 
--- src/modules/user/mutation.js
+++ src/modules/user/mutation.js
@@ -2,9 +2,9 @@ import { ApolloError } from 'apollo-server';
 
 const mutation = {
   loginUser: async (root, args, context) => {
-    const { email, password } = args;
-    const { dataSources: { userAPI } } = context;
     try {
+      const { email, password } = args;
+      const { dataSources: { userAPI } } = context;
       const { data } = await userAPI.loginUser({ email, password });
       return data;
     }
